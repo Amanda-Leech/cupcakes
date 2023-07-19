1befbc5f49fa518508240e68c10e688856d03419
amanda <amanda@devpipeline.com>
2023-07-19 19:28:54 -0400

    Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+    return (
+        <div>
+            <header>
+                <a href="/">Bobby Da' Baker</a>
+            </header>
+            <main>List Products</main>
+        </div>
+    );
+}
+
+export default App;
