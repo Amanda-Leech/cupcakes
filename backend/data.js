f923a4c2d03a16d919f1d526b07f18b72bf5d8c0
amanda <amanda@devpipeline.com>
2023-07-20 10:53:09 -0400

    add backend

---
+++ backend/data.js
@@ -0,0 +1,42 @@
+const data = {
+    products: [
+        {
+            name: 'White Ice Cream Cupcake',
+            slug: 'white-ice-cream-cupcake',
+            category: 'cupcakes',
+            image: '/images/white-cupcake.jpg', //679px x 829px
+            price: 30,
+            countInStock: 24,
+            brand: 'Bobby',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'One dozen vanilla ice cream cupcake with white frosting',
+        },
+        {
+            name: 'Strawberry Ice Cream Cupcake',
+            slug: 'strawberry-ice-cream-cupcake',
+            category: 'cupcakes',
+            image: '/images/pink-cupcake.jpg',
+            price: 30,
+            countInStock: 24,
+            brand: 'Bobby',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'One dozen strawberry ice cream cupcake with pink frosting',
+        },
+        {
+            name: 'Chocolate Ice Cream Cupcake',
+            slug: 'chocolate-ice-cream-cupcake',
+            category: 'cupcakes',
+            image: '/images/brown-cupcake.jpg',
+            price: 30,
+            countInStock: 24,
+            brand: 'Bobby',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'One dozen chocolate ice cream cupcake with brown frosting',
+        },
+    ],
+};
+
+export default data;
